55df9d3c357cf6c9934c3d3e2fb89884235b4d1f
arnau <arnau_mtez@hotmail.com>
2018-10-04 16:02:09 +0200

    Creacion y conexion con una base de datos.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var mysql   = require('mysql');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //Get all posts
+
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '1234',
+        database : 'tagless'
+    });
+
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+    });
+
+    connection.end();
+
+
   res.render('index', { title: 'Express', 'posts':[]});
 });
 
