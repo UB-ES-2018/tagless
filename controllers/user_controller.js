3ef9fc3de0faa55ce587cb5f23c1cbab6b81cc74
ziclon93 <ppizarhu43@alumnes.ub.edu>
2018-10-08 18:54:13 +0200

    control de error en signup hecho

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -7,14 +7,24 @@ exports.userController_Signup = function(u_email,u_name,u_pass) {
     var connection = dbconnection();
 
     connection.query('USE tagless;');
-    var sql = "INSERT INTO _user (email,username,pass) VALUES (?,?,?)";
-    var params = [u_email, u_name, u_pass ];
-    connection.query(sql, params ,function(err, result) {
-        if (err) throw err;
+    
+
+    userController_OnBD(u_email, u_name,function(err, content) {
+        if (err) {
+            return next("Mysql error, check your query");
+        } else {
+            if (content){
+                console.log("El usuario ya está dentro");
+            }
+            else{
+                var sql = "INSERT INTO _user (email,username,pass) VALUES (?,?,?)";
+                var params = [u_email, u_name, u_pass ];
+                connection.query(sql, params ,function(err, result) {
+                    if (err) throw err;
+                });
+            }
+        }
     });
-    var userExist = false;
-    userController_OnBD('pp@gmail.com', 'jojo',userExist,callback)
-    console.log(userExist);
     
 
 };
@@ -24,14 +34,14 @@ exports.userController_Login = function(u_name, u_pass) {
 
 };
 
-userController_OnBD =  function(u_email, u_name,userExist,callback){
+userController_OnBD =  function(u_email, u_name,callback){
 
     var connection = dbconnection();
 
     connection.query('USE tagless;');
     
 
-    console.log(connection.query('SELECT count(*) AS count FROM _user WHERE (_user.username = (?) OR _user.email = (?))',[u_name,u_email],function (err, result, fields){
+    connection.query('SELECT count(*) AS count FROM _user WHERE (_user.username = (?) OR _user.email = (?))',[u_name,u_email],function (err, result, fields){
         if (err) throw err;
         /*
         if (result[0]['count'] > 0){
@@ -39,8 +49,8 @@ userController_OnBD =  function(u_email, u_name,userExist,callback){
             userExist = true;
         }
         */
-        callback(null,null,result[0]['count']);
-    }));
+        callback(null,result[0]['count']>0);
+    });
 
 
 };
