c5e0ee5678225d41f3727c44c59b6f39b613f313
ziclon93 <ppizarhu43@alumnes.ub.edu>
2018-10-08 18:05:02 +0200

    comienzo de función check (no funciona)

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -9,15 +9,38 @@ exports.userController_Signup = function(u_email,u_name,u_pass) {
     connection.query('USE tagless;');
     var sql = "INSERT INTO _user (email,username,pass) VALUES (?,?,?)";
     var params = [u_email, u_name, u_pass ];
-    console.log(sql + params);
     connection.query(sql, params ,function(err, result) {
         if (err) throw err;
     });
+    var userExist = false;
+    userController_OnBD('pp@gmail.com', 'jojo',userExist,callback)
+    console.log(userExist);
     
 
 };
 
 exports.userController_Login = function(u_name, u_pass) {
+
+
+};
+
+userController_OnBD =  function(u_email, u_name,userExist,callback){
+
+    var connection = dbconnection();
+
+    connection.query('USE tagless;');
+    
+
+    console.log(connection.query('SELECT count(*) AS count FROM _user WHERE (_user.username = (?) OR _user.email = (?))',[u_name,u_email],function (err, result, fields){
+        if (err) throw err;
+        /*
+        if (result[0]['count'] > 0){
+            console.log("me rio en tu cara pedro");
+            userExist = true;
+        }
+        */
+        callback(null,null,result[0]['count']);
+    }));
 
 
 };
