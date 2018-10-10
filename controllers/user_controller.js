e32457e098d4832c3c917e062bf523e79c9a80e1
Laura Roigé <alesblaves@gmail.com>
2018-10-10 12:53:51 +0200

    User pass retrieved succesfully from database

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -13,11 +13,30 @@ exports.userController_Signup = function(u_email,u_name,u_pass) {
     connection.query(sql, params ,function(err, result) {
         if (err) throw err;
     });
-    
+
 
 };
 
 exports.userController_Login = function(u_name, u_pass) {
+    //retrieve user from database.
 
+    var connection = dbconnection();
+    var real_pass = '';
 
+    connection.query('USE tagless;');
+    var sql = 'SELECT pass FROM _user WHERE username = ?';
+    var query = connection.query(sql, [u_name], function (err, results) {
+        if (err) throw err;
+        console.log('res: ' + results);
+        real_pass = results[0].pass;
+        console.log('r_pass: ' + real_pass);
+    });
+    console.log('query: ' + query.sql);
+
+    //
+    // if (real_pass === u_pass) {
+    //     console.log("Logged");
+    // }
+    // else console.log("La contrasenya no és la correcta.");
+    // console.log(query._results);
 };
--- routes/users.js
+++ routes/users.js
@@ -4,37 +4,31 @@ var ctl_user = require('../controllers/user_controller');
 
 
 /* POST user login. */
-router.post('/login', function(req, res, next) {
-  //TODO
-  //Get data from request and log user
+router.post('/login', function (req, res, next) {
+    console.log(req.body);
+    console.log(req.body['username']);
+    console.log(req.body['password']);
 
-  console.log(req.body);
-  console.log(req.body['username']);
-  console.log(req.body['password']);
-  res.send("Loged");
-});
+    //Get data from request and log user
+    ctl_user.userController_Login(req.body['username'], req.body['password']);
 
-/* POST user login. */
-router.post('/login', function(req, res, next) {
-  //TODO
-  //Get data from request and log user
-  res.send("Loged");
+    res.send("Loged");
 });
 
 /* POST user register. */
-router.post('/signup', function(req, res, next) {
-  //TODO
-  //Get data from request and log user
-  ctl_user.userController_Signup(req.body['email'],req.body['username'], req.body['password']);
+router.post('/signup', function (req, res, next) {
+    //TODO
+    //Get data from request and log user
+    ctl_user.userController_Signup(req.body['email'], req.body['username'], req.body['password']);
 
-  res.send("Registered");
+    res.send("Registered");
 });
 
 
 /* GET user singup. */
-router.get('/signup', function(req, res, next) {
+router.get('/signup', function (req, res, next) {
 
-  res.render('signup');
+    res.render('signup');
 //TODO
 //Get data from request and create user
 //res.render('signup');
