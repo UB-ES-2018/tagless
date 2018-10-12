6e8ee5476a518d7def67b737a1566965fe38318c
Jorge Tomé <jorgetomeb@gmail.com>
2018-10-12 14:12:42 +0200

    Sequelize connection working successfuly

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Sequelize = require('sequelize');
 var models = require('./config/models')
+var sequelizeConnection = require("./config/sequelizeConnection");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,6 +13,28 @@ var postRouter = require('./routes/post');
 
 var app = express();
 var mod = models();
+var sequelize = sequelizeConnection.sequelize; //instance to query
+
+//test ----
+const User = sequelize.define('User',{
+  userId : Sequelize.INTEGER,
+  email: Sequelize.STRING,
+  username: Sequelize.STRING,
+  pass: Sequelize.STRING,
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE,
+})
+
+sequelize.query('SELECT * FROM Users')
+    .then(user => console.log(user));
+
+
+var data = User.findAll({
+    attributes: ['username', 'pass']});
+
+console.log(data.valueOf());
+
+//test ----
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
