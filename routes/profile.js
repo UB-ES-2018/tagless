fb889c2a21b1b699e01c3e42bdf155dd4b4cf538
Daniel Lopez <daniellopez.macia@gmail.com>
2018-10-18 22:55:17 +0200

    cambiamos la imagen, falta descripcion

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,7 @@ var router  = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //Get all posts
-  res.render('profile', { username: 'Aradan', description: 'Ejemplo de descripcion', posts: [] })
+  res.render('profile', { username: 'Aradan', imageURL: '', description: 'Ejemplo de descripcion', posts: [] })
 });
 
 module.exports = router;
