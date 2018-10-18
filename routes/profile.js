7224d09192bba0a9808f920966d6e9371380cda1
Daniel Lopez <daniellopez.macia@gmail.com>
2018-10-18 15:03:13 +0200

    creada pagina de usuario

---
+++ routes/profile.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router  = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //Get all posts
+  res.render('profile', { username: 'Aradan', description: 'Ejemplo de descripcion', posts: [] })
+});
+
+module.exports = router;
