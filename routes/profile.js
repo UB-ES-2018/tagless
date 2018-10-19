811e71cd3e133d09ba7a9e01c788dfddc9744f7d
Daniel Lopez <daniellopez.macia@gmail.com>
2018-10-19 23:39:15 +0200

    ya estan la ventana fea de settings, falta hacer los posts y gets bien

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router  = express.Router();
+var ctl_user = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //Get all posts
-  res.render('profile', { username: 'Aradan', imageURL: '', description: 'Ejemplo de descripcion', posts: [] })
+	res.render('profile', { username: 'Aradan', imageURL: '', description: 'Ejemplo de descripcion', posts: [] })
+});
+
+
+router.get('/settings', function(req, res, next) {
+
+	res.render('profile_settings', { username: 'Aradan', imageURL: '', description: 'Ejemplo de descripcion', posts: [] })
+});
+
+router.post('/settings', function(req, res, next) {
+
+	//hacer los cambios 
+
+	res.render('profile', { username: 'Aradan', imageURL: '', description: 'Ejemplo de descripcion', posts: [] })
 });
 
 module.exports = router;
