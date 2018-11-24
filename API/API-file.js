c0e562ef492be7d546b7a3e1648951bc6f400d6b
ziclon93 <ppizarhu43@alumnes.ub.edu>
2018-11-24 12:27:04 +0100

    API is happening

---
+++ API/API-file.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+var router = express.Router();
+var comment_ctl = require('../controllers/comment_controller');
+
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var threadRouter = require('./routes/thread');
 var commentsRouter = require('./routes/comments');
+var APIRouter = require('./API/API-file');
 
 
 var app = express();
@@ -83,6 +84,7 @@ app.use('/users', usersRouter);
 app.use('/thread', threadRouter);
 app.use('/static', express.static('public'));
 app.use('/static/open-iconic', express.static('node_modules/open-iconic'));
+app.use('/API', APIRouter);
 
 
 //test
