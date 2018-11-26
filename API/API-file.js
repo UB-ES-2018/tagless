a52aa6875481bb43cc680896da38ea92f2f389a5
ziclon93 <ppizarhu43@alumnes.ub.edu>
2018-11-26 20:20:28 +0100

    get of user

--- API/API-file.js
+++ API/API-file.js
@@ -5,7 +5,7 @@ var ctl_comments = require('../controllers/comment_controller');
 var ctl_user = require('../controllers/user_controller');
 
 //Middleware to check API key
-async function ayncCheckAPIKey(req,res,next){
+async function asyncCheckAPIKey(req,res,next){
     try{
         var result = await ctl_user.getUserByAPIKey(req.headers['api-key'] );
         if (result){
@@ -21,8 +21,8 @@ async function ayncCheckAPIKey(req,res,next){
 }
 
 
-router.get('/users', ayncCheckAPIKey ,function (req, res, next) {
-    ctl_user.getUserByAPIKey(req.headers['api-key'] )
+router.get('/users', asyncCheckAPIKey ,function (req, res, next) {
+    ctl_user.getUserByAPIKey(req.headers['api-key'])
         .then(function(user){
             if(user){
                 res.json({
@@ -47,6 +47,55 @@ router.get('/users', ayncCheckAPIKey ,function (req, res, next) {
             res.status(500).send("Internal server error");
         });
 });
+
+
+
+router.get('/user/:user',asyncCheckAPIKey, function(req,res,next){
+    
+    var username = req.url.substr(6,req.url.length);
+    ctl_user.getUserByUsername(username).then( user =>{
+        if(user){
+            res.json({
+                apiKey : user.apiKey,
+                id: user.id,
+                username: user.username,
+                password: user.pass,
+                email: user.email,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt,
+                privacity: user.privacity,
+                description: user.description,
+                pictureLink: user.pictureLink,
+            })
+        }
+        else{
+            var userid = parseInt(username);
+            ctl_user.getUserById(userid).then( user =>{
+                if(user){
+                    res.json({
+                        apiKey : user.apiKey,
+                        id: user.id,
+                        username: user.username,
+                        password: user.pass,
+                        email: user.email,
+                        createdAt: user.createdAt,
+                        updatedAt: user.updatedAt,
+                        privacity: user.privacity,
+                        description: user.description,
+                        pictureLink: user.pictureLink,
+                    })
+                }
+                else{
+                    res.json({
+                        success: false,
+                    })
+                }
+            });
+        }
+    });
+
+});
+
 
 
 /*
@@ -81,7 +130,7 @@ router.get('/users', ayncCheckAPIKey ,function (req, res, next) {
  *
  * Return: True (if signed up) False (user already signed up)
  */
-router.post('/signup', ayncCheckAPIKey, function (req, res, next) {
+router.post('/signup', asyncCheckAPIKey, function (req, res, next) {
     ctl_user.userController_Signup(req.body['email'], req.body['username'], req.body['password'])
         .then(function(success){
             if(success){
