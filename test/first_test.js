be8a830dda6b52a11c02461027443f201d32f0f4
arnau <arnau_mtez@hotmail.com>
2018-11-27 19:53:44 +0100

    [Testing] 	- Created the skeleton for testing

---
+++ test/first_test.js
@@ -0,0 +1,9 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+it('Main page status', function(done) {
+    request('http://localhost:3000' , function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
