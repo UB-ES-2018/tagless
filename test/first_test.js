9f630e7b11b78a17b23af0eee1ee2548bccb0768
arnau <arnau_mtez@hotmail.com>
2018-11-27 20:27:28 +0100

    .

--- test/first_test.js
+++ test/first_test.js
@@ -1,9 +1,19 @@
 var expect  = require('chai').expect;
 var request = require('request');
+var ctl_user = require('../controllers/user_controller');
+
+
+
 
 it('Main page status', function(done) {
     request('http://localhost:3000' , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
+});
+
+it('Signup de un usuario', function(){
+   var results = ctl_user.userController_Signup("arnau@gmail.com", "arnau114", "arnau");
+   console.log(results);
+   expect(results).to.equal(false);
 });
