5cb7670a9da6181be217a724eafc9346ce0846b1
ziclon93 <ppizarhu43@alumnes.ub.edu>
2018-12-18 18:36:15 +0100

    ElasticSearch in heroku v1

--- config/elasticsearch/elasticsearchConnection.js
+++ config/elasticsearch/elasticsearchConnection.js
@@ -2,7 +2,8 @@ const elasticsearch = require("elasticsearch");
 var client;
 
 client = new elasticsearch.Client({
-  host: 'localhost:9200'
+  host: 'cedar-916953153.eu-west-1.bonsaisearch.net'
+  //:9200
 });
 
 module.exports = client;
